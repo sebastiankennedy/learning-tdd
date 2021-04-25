c582d08e4a529bc23208b300eaa0e5f53456d865
sebastiankennedy <sebastiankennedy@foxmail.com>
2021-04-25 09:09:29 +0800

    feat: ViewQuestionsTest.php

---
+++ LearnkuTDD/tests/Feature/ViewQuestionsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewQuestionsTest extends TestCase
+{
+    /** @test */
+    public function user_can_view_questions()
+    {
+        // 抛出异常
+        $this->withoutExceptionHandling();
+
+        // 假设路由存在、访问路由
+        $test = $this->get('/questions');
+
+        // 正常返回 200
+        $test->assertStatus(200);
+    }
+
+    /** @test */
+    public function user_can_view_a_single_question()
+    {
+        // 创建一个问题
+        $question = Question::factory()->create();
+
+        // 访问连接
+        $test = $this->get('/question/'.$question->id);
+
+        // 那么应该看到问题的内容
+        $test->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
+    }
+}
